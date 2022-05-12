d7be301de88e233213c588e5d95ee978824427ea
djkeu <mrkooij@gmail.com>
2022-05-12 11:34:58 +0200

    Create 04_birthday_banner.kt

---
+++ 04_birthday_banner.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // #4: Print a birthday banner with a border
+    printBorder()    
+    println("Happy Birthday, Jhansi!")
+    printBorder()
+}
+
+fun printBorder() {
+    println("=======================")
+    
+}
