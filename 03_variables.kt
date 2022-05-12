1d3e8c38cf623240519f81f2febb903e45253723
djkeu <mrkooij@gmail.com>
2022-05-12 11:34:39 +0200

    Create 03_variable.kt

---
+++ 03_variables.kt
@@ -0,0 +1,21 @@
+fun main() {
+    // #3: Create and use variables
+    val age = 5 * 365
+    val name = "Rover"
+    
+    println("Happy birthday, ${name}!")
+    
+    // Let's print a cake!
+	println("   ,,,,,   ")
+	println("   |||||   ")
+	println(" ========= ")
+	println("@@@@@@@@@@@")
+	println("{~@~@~@~@~}")
+	println("@@@@@@@@@@@")
+    
+    // This prints an empty line
+	println("")
+    
+    println("You are already ${age} days old!")
+    println("${age} days old is the very best age to celebrate!")
+}
